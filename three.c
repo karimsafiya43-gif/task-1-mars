74eb320bae06e432c58013c236a8c1646aab0c0a
karimsafiya43-gif <karimsafiya43@gmail.com>
2026-04-09 12:00:36 +0530

    Update three.c

--- three.c
+++ three.c
@@ -4,17 +4,18 @@
 
 void letter(char arr[]){
     int size=strlen(arr);
-     char b[100];
-    
+    //temporary variable to store the value
+    char b[100];
+    //moves to all the. character of the string till it gets /0 value
     for (int i = 0; arr[i] != '\0'; i++) {
         
-
+//we get this using ascii value of the characters from every ascii value its (position+1) is getting subtracted
     b[i]=(char)((int)(arr[i])-(i+1));
    
 }
 b[size]='\0';
 
-
+//Print all the characters of b as a string 
     printf("%s",b);
 }
 
@@ -22,6 +23,7 @@ int main(){
     char arr[100];
     printf("enter the message\n");
     scanf("%s",arr);
+    //Convert the input to upper case
     for(int i=0;i<strlen(arr);i++){
        arr[i] = (char)toupper((unsigned char)arr[i]);
     }
